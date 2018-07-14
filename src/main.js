3e93aa3e72ae504f9c941728c0b58ac46971ea99
Filip Brzozowski <fbrzozowski@fiberfuse.co.uk>
2018-07-15 00:25:53 +0200

    Add electron edit menu

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ const { app, BrowserWindow, Menu } = require('electron')
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win;
+
 function createWindow() {
     // Create the browser window.
     win = new BrowserWindow({ width: 800, height: 600 })
@@ -16,6 +17,29 @@ function createWindow() {
         // when you should delete the corresponding element.
         win = null
     });
+
+    var template = [{
+        label: "Application",
+        submenu: [
+            { label: "About Application", selector: "orderFrontStandardAboutPanel:" },
+            { type: "separator" },
+            { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); } }
+        ]
+    }, {
+        label: "Edit",
+        submenu: [
+            { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+            { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+            { type: "separator" },
+            { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+            { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+            { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+            { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+        ]
+    }];
+
+    Menu.setApplicationMenu(Menu.buildFromTemplate(template));
+
 }
 
 // This method will be called when Electron has finished
